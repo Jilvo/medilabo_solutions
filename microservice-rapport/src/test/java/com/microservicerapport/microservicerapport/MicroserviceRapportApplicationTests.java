4a27fa249a81924f7f9c42c88ae32bb8a2705e48
Jilvo <jmillot41@gmail.com>
2024-04-28 11:07:44 +0200

    start microservice Sprint Three

---
+++ microservice-rapport/src/test/java/com/microservicerapport/microservicerapport/MicroserviceRapportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicerapport.microservicerapport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRapportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
